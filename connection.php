def6f79d85637ac7da16e944c0487f6139992c6e
YAROU MHR <kingmhr39@gmail.com>
2024-02-29 04:29:16 +0100

    Connection à la base de donnée en PDO

--- connection.php
+++ connection.php
@@ -2,3 +2,10 @@
 
 #FR: Ce fichier contient la connexion à la base de données.
 #AN: This file contains the database connection.
+
+try {
+    $bdd = new PDO('mysql:host=localhost; dbname=crud; charset=utf8', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('la connexion a échoué:' . $e->getMessage());
+}
